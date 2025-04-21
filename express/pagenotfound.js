8dbb498f86b9e53c52b1475d0dce0854dcb3abc0
Umme Aiman <122363026+umme-aimany@users.noreply.github.com>
2025-04-22 02:19:15 +0500

    Update pagenotfound.js

--- express/pagenotfound.js
+++ express/pagenotfound.js
@@ -2,15 +2,31 @@ const express = require('express');
 const path = require('path'); 
 const app = express();
 
-app.get('/', (request, response) => {
+const publicPath = path.join(__dirname, "public");
 
-   respionse.sendFile
-   ///////////isma home about or page not founde ka page bnai ga  
- 
- });
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.use(express.static(publicPath));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
+
+app.get("/home", (req, res) => {
+  res.sendFile(path.join(publicPath, "home.html"));
 });
 
 
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(publicPath, "about.html"));
+});
+
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(publicPath, "pagenotfound.html"));
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
